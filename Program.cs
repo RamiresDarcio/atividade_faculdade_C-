74b8b03a126ff695ad3da0d2c7e4a0cc4d065ef4
ramires_felino <ramiresdarcio2104@gmail.com>
2026-03-17 09:03:03 -0300

    atividade_de_hoje

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+try
+{
+ int divisor = 0;
+ int resultado = 10 / divisor;
+ Console.WriteLine("resultado"+resultado);
+
+}
+catch (DivideByZeroException e)
+{
+ Console.WriteLine($"erro matematica:{e.Message}");
+}
+
+    
